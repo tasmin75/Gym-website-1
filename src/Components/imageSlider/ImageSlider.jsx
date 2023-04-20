cafeda577e264022b4da6a303502a923bd7a10eb
Tasmin <tansarijkm25@gmail.com>
2023-04-20 22:12:56 +0530

    project is done

--- src/Components/imageSlider/ImageSlider.jsx
+++ src/Components/imageSlider/ImageSlider.jsx
@@ -1,4 +1,4 @@
-import styles from './ImageSlider.module.css';
+import styles from "./ImageSlider.module.css"
 import { useEffect, useState } from 'react';
 import { Footer } from '../footer/Footer';
 
@@ -24,7 +24,7 @@ export function ImageSlider() {
       ];
       
 
-    const [image, setImage] = useState(imgArr[0]);
+    // const [image, setImage] = useState(imgArr[0]);
     const [index, setIndex] = useState(0);
 
 
